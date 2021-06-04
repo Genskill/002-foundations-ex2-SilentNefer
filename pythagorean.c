6330f7e499892561e4332d03ceffb7c99cbddd36
SilentNefer <84319452+SilentNefer@users.noreply.github.com>
2021-06-04 21:40:53 +0530

    Create pythagorean.c

---
+++ pythagorean.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+void pyth(float, float, float);
+
+void pyth(float num1, float num2, float num3)
+{
+    float sum;
+    sum=(num1*num1)+(num2*num2);
+    if (sum==(num3*num3))
+    {
+        printf("Yes");
+    }
+    else
+    {
+        printf("No");
+    }
+}
+
+int main()
+{
+    float a, b, c, num1, num2, num3;
+    printf("Side 1 ");
+    scanf("%f", &a);
+    printf("Side 2 ");
+    scanf("%f", &b);
+    printf("Side 3 ");
+    scanf("%f", &c);
+    
+    if (a<b)
+    {
+        if (b<c)
+        {
+            num3=c;
+            num1=a;
+            num2=b;
+            pyth(num1, num2, num3);
+        }
+        else if (b>c)
+        {
+            num3=b;
+            num1=a;
+            num2=c;
+            pyth(num1, num2, num3);
+        }
+        else
+        {
+            printf("No");
+        }
+        
+    }
+    else if (a>b)
+    {
+        if(a>c)
+        {
+            num3=a;
+            num2=b;
+            num1=c;
+            pyth(num1, num2, num3);
+        }
+        else if (c>a)
+        {
+            num3=c;
+            num1=a;
+            num2=b;
+            pyth(num1, num2, num3);
+        }
+        else
+        {
+            printf("No");
+        }
+    }
+    else
+    {
+        printf("No");
+    }
+}
